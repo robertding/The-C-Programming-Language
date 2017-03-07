18a9bea320377dd04b72b6b63b8b90b1fa83dfca
robertding <robertdingx@gmail.com>
2017-03-07 22:39:01 +0800

    FIX - change name to main'

--- Chapter-1/1-1/hello-world.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main(char argc, char *argv){
-    printf("hello world");
-}
--- Chapter-1/1-2/hello-world.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main(char argc, char *argv){
-    printf("hello world \a \b \c \d \e \f \g \h \i \j \k \l \m \n \o");
-}
